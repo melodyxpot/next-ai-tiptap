e21da3ffbc2b4e97dc2ac362c73aeee82789c73f
melodyxpot <melodyxpot@gmail.com>
2024-09-13 02:54:58 -0700

    :fire: useless components

--- utils.ts
+++
@@ -1,7 +0,0 @@
-import wrapFetch from "fetch-retry";
-
-export const fetchWithRetry = wrapFetch(fetch);
-
-export const exponentialBackoff = function (attempt: number) {
-	return Math.pow(2, attempt) * 1000; // 1000, 2000, 4000
-};
